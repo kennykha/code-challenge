27fcdcbf640c7b856f24ba168f8b7c2fa201c5ae
kennykha <kennykha@gmail.com>
2021-06-29 09:34:04 -0700

    Adding basic input form, data change handling for username and password

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,17 +1,37 @@
 import Head from 'next/head';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import styles from 'src/styles/create_account.module.scss';
 
-export default function CreateAccount() {
-  async function handleSubmit(evt: FormEvent) {
-    evt.preventDefault();
-    const response = await fetch('/api/create_new_account', {
-      method: 'POST',
-      body: JSON.stringify({}),
-    });
+// export default function CreateAccount() {
+//   async function handleSubmit(evt: FormEvent) {
+//     evt.preventDefault();
+//     const response = await fetch('/api/create_new_account', {
+//       method: 'POST',
+//       body: JSON.stringify({}),
+//     });
 
-    console.log(await response.json());
-  }
+//     console.log(await response.json());
+//   }
+
+
+
+  export default function CreateAccount() {
+    async function handleSubmit(evt: FormEvent) {
+      evt.preventDefault();
+
+      console.log(username, password)
+    }
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleDataChange = (type) => {
+      if (type === "username") {
+        setUsername((document.getElementById("username") as HTMLInputElement).value)
+      } else if (type === "password") {
+        setPassword((document.getElementById("password") as HTMLInputElement).value)
+      }
+    }
 
   return (
     <>
@@ -20,6 +40,8 @@ export default function CreateAccount() {
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
+          <input type='text' id='username' placeholder="Username" value={username} onChange={() =>handleDataChange('username')}/>
+          <input type='text' id='password' placeholder="Password" value={password} onChange={() =>handleDataChange('password')}/>
           <button>Create Account</button>
         </form>
       </article>
